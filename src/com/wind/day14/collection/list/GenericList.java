2fa6fad707bd9f64e45aa8e29e9b55f339c4e6ab
felixhsm <felixhsm9410@gmail.com>
2024-12-16 12:28:45 +0900

    [ADD] Day14 컬렉션과 제네릭
    1. List: 순서를 유지하고 중복 저장될 수 있는 자료구조,맛집 대기자 리스트와 같은 형태
    2. Set: 순서는 중요하지 않으면 중복을 저장할 수 없음. 집합이 그 예시이며 중복을 제거할 때 사용됨.
    3. Map: 키와 밸류로 이루어진 쌍으로 데이터를 저장하며 if문과 같은 부분을 저장소로 저용하여 프로그래밍할 수 있음.

---
+++ src/com/wind/day14/collection/list/GenericList.java
@@ -0,0 +1,26 @@
+package com.wind.day14.collection.list;
+
+public class GenericList<T> {
+	Object [] objects;
+	int size;
+	
+	public GenericList() {
+		objects = new Object[3];
+		size = 0;
+	}
+	
+	public void add(T object) {
+		objects[size] = object;
+		size++;
+	}
+	public T get(int index) {
+		return (T)objects[index]; 
+	}
+	public int size() {
+		return size;
+	}
+	public void claer() {
+		objects = new Object[3];
+		size = 0;
+	}
+}
