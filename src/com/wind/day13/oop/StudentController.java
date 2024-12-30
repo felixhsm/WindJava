781a2249cc19d03812ec18ca5a64cf1880db131e
felixhsm <felixhsm9410@gmail.com>
2024-12-30 12:27:13 +0900

    [ADD] Day13 Student 클래스에 주석 달아서 내용 추가

--- src/com/wind/day13/oop/StudentController.java
+++ src/com/wind/day13/oop/StudentController.java
@@ -1,25 +1,49 @@
 package com.wind.day13.oop;
 
+/*
+ * View와 Manage 서로 메세지 전달을 통해서 상호작용을 해야하는데
+ * 중간에 있어서 그 View와 Manage 연결하는 역할을 하는 클래스임.
+ * View -> Controller -> Manage : 데이터 입력 후 저장
+ * Manage -> Controller -> View : 데이터 로드 후 출력
+ */
+
 public class StudentController {
 	public static void main(String[] args) {
 		StudentView view = new StudentView();
 		StudentManage manage = new StudentManage();
 		exit:
 		while(true) {
+			// 메뉴를 출력하기 위해서 mainMenu()를 호출하고
+			//입력받은 숫자가 리턴될 것이기 때문에 int 변수로 받음
 			int choice = view.mainMenu();
+			// 해당 선택에 따라 다르게 동작하도록, 하기위함
+			// switch ~ case문
 			switch(choice) {
 			case 1:
+				// 1번 동작
+				// 입력을 받을 때는 view부터
+				// 넘겨받은inputStudent객체를 저장하기 위해서
+				// manage에 있는 insertStudent()를 호출하고 전달해줌
+				// sutdent를 전달해줌
 				Student student = view.inputStudentData(); 
 				manage.inserStudent(student);
 				break;
 			case 2: 
+				// 2번 동작
+				// 성적 출력을 하려면 그동안 저장했었던 데이터를 로드해야함
+				// manage에 있는 getStudent()메소드를 호출하면
+				// 모든 데이터가 넘어올 것임. 그 데이터의 타입은 Student[]
+				// 이기 때문에 Student[] 변수로 받음.
+				// 입력 받은 정보를 출력할 때는 manage부터
 				Student [] students = manage.getStudents();
 //				for(int i = 0; i < 3; i++) {
 //					view.printStudentData(students[i]);
 //				}
-				
+				// 넘어온 데이터를 모두 출력하기 위해서
+				// 데이터를 전달해줌. view에 있는 printStudentData()
+				// 메소드에 전달해줌.
 				view.printStudentData(students[0]);
-//				view.printStudentData(students[1]);
+				view.printStudentData(students[1]);
 //				view.printStudentData(students[2]);
 				break;
 			case 3: 
--- src/com/wind/day13/oop/StudentManage.java
+++ src/com/wind/day13/oop/StudentManage.java
@@ -1,9 +1,16 @@
 package com.wind.day13.oop;
 
+import java.util.List;
 import java.util.Scanner;
 
 // 저장하고 로드하는 기능 클래스
+/*
+ * 최종저장소를 가지고 있으면서 
+ * View에서 입력받은 데이터를 받아서
+ * 저장하거나 필요할 때 로드해주는 클래스로 Manage클래스
+ */
 public class StudentManage {
+	private List<Student>sList;
 	private Student[]students;
 	private int index;
 	
@@ -11,15 +18,24 @@ public class StudentManage {
 		students = new Student[3];
 		index = 0;
 	}
+	/*
+	 * insertStudent() 메소드는 Student객체를
+	 * 전달받아서 최종저장소인 객체배열(리스트)에
+	 * 저장하는 메소드임
+	 * Controller에서 호출함. 호출할때 student객체를
+	 * 넘겨줌. 넘겨주니까 받아야 되고 받아야 되니까
+	 * 매개변수 작성.
+	 */
+	public void inserStudent(Student student) {
+		//sList.add(Student);
+		students[index] = student;
+		index++;
+	}
 //		String name = sc.next();
 //		students[index].setName(name);
 //		students[index].setKor(sc.nextInt());
 //		students[index].setEng(sc.nextInt());
 //		students[index].setMath(sc.nextInt());
-	public void inserStudent(Student student) {
-		students[index] = student;
-		index++;
-		
 		
 //		for(int i = 0; i < students.length; i++) {
 //			students[i] = new Student();
@@ -29,7 +45,18 @@ public class StudentManage {
 //			students[i].math = sc.nextInt();
 //			
 //		}
-	}
+	/*
+	 * 리턴형은 왜 Student[] 인가?
+	 * 이 메소드가 하는 역할은?
+	 * 저장소,객체배열 또는 리스트가 가지고 있은
+	 * 모든 데이터를 로드하는 역할
+	 * Contrller에서 호출되었을 때 모든 데이터가 넘어 올것을 기대함.
+	 * 그 데이터를 넘겨주어야하고
+	 * 넘겨주려면 return students; 또는 return sList;
+	 * 를 써야함.studetns의 타입이 Student[]이니까
+	 * void ->Student[]로 변경해주어야 하기 때문에
+	 * 리턴형ㅇ Student[]임.
+	 */
 	public Student []getStudents(){
 		return students;
 	}
