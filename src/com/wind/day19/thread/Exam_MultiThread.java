54b1b2efd629fce1a3709a7662117c9bf5f410dd
felixhsm <felixhsm9410@gmail.com>
2024-12-30 12:26:34 +0900

    [ADD] Day19 쓰레드 동기화 설명 및 예제코드
    1. 쓰레드 동기화의 필요성 및 적용된 예제 코드

---
+++ src/com/wind/day19/thread/Exam_MultiThread.java
@@ -0,0 +1,56 @@
+package com.wind.day19.thread;
+
+class printNumbers implements Runnable{
+
+	@Override
+	public void run() {
+		for(int i = 1; i < 6; i++){
+			System.out.println("숫자 : " + i);
+			try {
+				Thread.sleep(1000); // 밀리세크초를 전달값으로 받음, Checked Exception 예외 강제
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+	}
+}
+}
+
+class downloadFiles implements Runnable{
+
+	@Override
+	public void run() {
+		String []files = {"document.pdf", "image.jpg", "video.mp4"};
+		for(String file:files) {
+			System.out.println(file + "다운로드 중..");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
+
+public class Exam_MultiThread {
+	public static void main(String[] args) {
+		Thread nThread = new Thread(new printNumbers());
+		Thread dThread = new Thread(new downloadFiles());
+		nThread.start();
+		dThread.start();
+		
+		// 기존 싱글 스레드
+//		printNumbers();
+//		downloadFiles();
+	}
+	
+//	private static void downloadFiles() {
+//		
+//		
+//	}
+//	
+//	private static void printNumbers() {
+//		
+	}
